6d3210b45d3d74d6212b17a0fb789f6d64418459
John Daniels <adeyemijohndaniels@gmial.com>
2022-05-12 14:38:13 +0100

    extending to the db

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const path = require("path")
 
 //routers
 const elementsRouter = require("./routers/elements")
+const Element = require("./models/element")
 
 // additional pages for (404)
 const public = path.resolve(__dirname, "../public")
@@ -25,13 +26,41 @@ app.use(express.json())
 
 app.use("/api/elements", elementsRouter)
 
-// choose your own routes
+// 404
 app.use("/*", (req, res) => {
-  res.redirect("http://google.com")
+  res.redirect("https://github.com/John-Daniels/periodic-table-api")
 })
 
-const main = () => {
-  const Elements = require("./models/element")
+const main = async () => {
+  const fs = require("fs")
+
+  const dbDir = path.resolve(__dirname, "../data.json")
+  const dataBuf = fs.readFileSync(dbDir)
+  const _elements = JSON.parse(dataBuf)
+
+  // List < Map < String,
+  //   dynamic >>
+  _elements.forEach(async (element) => {
+    if (element.yearDiscovered === "Ancient") {
+      element.yearDiscovered = null
+    }
+    function isArray(o) {
+      return Object.prototype.toString.call(o) === "[object Array]"
+    }
+    if (isArray(element.atomicMass)) {
+      element.atomicMass = element.atomicMass[0].toString()
+    }
+    const newElement = Element(element)
+    const e = await newElement.save()
+    console.log(`copied ${e.symbol}`)
+  })
+
+  // for (var e of _elements) {
+  //   console.log(Object.keys)
+  //   break
+  // }
 }
+
+main()
 
 app.listen(port, () => console.log(`server is up at port ${port}`))
